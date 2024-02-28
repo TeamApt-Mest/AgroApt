a82edcecd6400c368fa912959174be50c868210d
darleynartey <barbaradnartey@gmail.com>
2024-02-28 09:24:51 +0000

    Add hero session and featured products

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import fruitsveg from '../assets/images/fruitsveg.png'
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <div className="hero-image">
+          <img src={fruitsveg} alt="fruitsveg" />
+        </div>
+        <div className="hero-text">
+          <h3 className="welcome-message">Welcome to AgroApt</h3>
+          <h1>Fresh & Healthy Organic Food</h1>
+          <p className="order-message">Free shipping on all your order, we deliver, you enjoy.</p>
+          <button className='purchase-message'>Shop now<span></span></button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
