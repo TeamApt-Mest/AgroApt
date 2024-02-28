1a0b9fe1fbf32999ecf0f1a21afd2271252ecf71
ednaakpene <edvalerie18@gmail.com>
2024-02-28 11:10:03 +0000

    edited cards.jsx code

--- src/components/Testimonial/Cards.jsx
+++ src/components/Testimonial/Cards.jsx
@@ -6,10 +6,10 @@ const Cards = (props) => {
     <>
       <div className="container">
         <div className="card ">
-          <img className="quote-icon" src={props.image} />
+          <img className="quote-icon" src={props.image} alt="quote icon" />
           <p className="card-text">{props.cardtext}</p>
         </div>
-        <img className="reviewer" src={props.person} />
+        <img className="reviewer" src={props.person} alt="person" />
         <h4 className="reviewer-name">{props.name}</h4>
         <p>{props.position}</p>
       </div>
