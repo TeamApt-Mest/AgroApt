7e535709c3d7d9fed1d1605e9e6633012e329e54
ednaakpene <edvalerie18@gmail.com>
2024-02-29 12:51:41 +0000

    linked the signup page to the button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import LandingPage from './pages/landing/Landing';
+import LandingPage from './Pages/landing/landing.jsx';
 import { Routes, Route } from "react-router-dom";
-import AboutUs from './pages/aboutUs.jsx';
-import BuyNow from './pages/buyNow.jsx';
-import SellNow from './pages/sellNow.jsx';
-import Login from './pages/login.jsx';
+import AboutUs from './Pages/aboutus.jsx';
+import BuyNow from './Pages/buynow.jsx';
+import SellNow from './Pages/sellnow.jsx';
+import Login from './Pages/Login.jsx';
+import Signup from './Pages/signup/Signup.jsx';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/buyNow" element={<BuyNow />} />
       <Route path="/sellNow" element={<SellNow />} />
       <Route path="/login" element={<Login />} />
+      <Route path='/signup' element={<Signup/>} />
     </Routes>
 
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/AgroApt Logo.jpg";
 import icon from "../../assets/cart-icon.png";
@@ -12,30 +12,44 @@ const NavBar = () => {
           <img className="appLogo" src={logo} alt="logo of app" />
         </div>
         <form className="d-flex formInput" role="search">
-        <input className="form-control me-1 input" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-success but" type="submit">Search</button>
-      </form>
-    <div>
-       <img className="cartIcon" src={icon} alt="" />
-     </div>
+          <input
+            className="form-control me-1 input"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-success but" type="submit">
+            Search
+          </button>
+        </form>
+        <div>
+          <img className="cartIcon" src={icon} alt="" />
+        </div>
       </section>
       <section className="secondSection">
         <ul className="listItems">
-<li className="list">Home</li>
-<li className="list">About Us</li>
-<li className="list">Buy Now</li>
-<li className="list">Sell Now</li>
+          <li className="list">
+            {" "}
+            <Link class="active" to="/home">
+              Home
+            </Link>
+          </li>
+          <li className="list">About Us</li>
+          <li className="list">Buy Now</li>
+          <li className="list">Sell Now</li>
         </ul>
         <div className="buttons"></div>
         <div className="buttonSection">
           <button className="loginButton">Login</button>
-          <button className="signupButton">Sign Up</button>
+          <Link to="/signup">
+            <button className="signupButton">Sign Up</button>
+          </Link>
         </div>
       </section>
     </nav>
     // <div className="container">
     //   <img className="logo" src={logo} alt="" />
-      
+
     //   <div>
     //     <input type="text" placeholder="Search.." />
     //     <input type="submit" />
