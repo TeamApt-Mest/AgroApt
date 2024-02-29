e5de1ec3356b365fab671080ba9b20a78fc923b7
Konkuri1 <konkurievarest363@gmail.com>
2024-02-29 09:28:36 +0000

    refactored code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 // import {  Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/Navbar.jsx";
 import LandingPage from "./pages/landing";
+// import AboutUs importimport { RouterProvider } from "react-router-dom";
+//  { createBrowserRouter } from "react-router-dom";
+//  from "./pages/landing/aboutUs";
+
 // import aboutUs from "./pages/aboutUs";
 // import buyNow from "./pages/buyNow";
 // import sellNow from "./pages/sellNow";
 // import login from "./pages/login";
 
+// const router = createBrowserRouter([{
+//   path:"/", element: <AboutUs/>}
+// ])
 function App() {
   return (
    <>
       <NavBar />
       <LandingPage/>
-      {/* <Routes>
+      
+         {/* <Routes>
         <Route path="/home" element={<LandingPage />} />
         <Route path="/aboutUs" element={<aboutUs />} />
         <Route path="/buyNow" element={<buyNow />} />
         <Route path="/sellNow" element={<sellNow />} />
-        <Route path="/login" element={<login />} />
-      </Routes> */}
+        <Route path="/login" element={<login />} /> 
+      </Routes>   */}
       </>
   );
 }
